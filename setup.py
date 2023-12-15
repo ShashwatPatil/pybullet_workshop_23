e743990202fcd8ddb37bb0b4d429d241d53e0b07
SGP <shashwatpatil974@gmail.com>
2023-12-15 19:06:28 +0530

    adding the actual package

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setuptools.setup(
     name='pybullet_workshop_23',
     version='0.0.1',
     description="A OpenAI Gym Env for pybullet and vision",
-    long_description=Path("README.md").read_text(),
+    long_description=Path("Readme.md").read_text(),
     long_description_content_type="text/markdown",
     packages=setuptools.find_packages(include="pybullet_workshop_23*"),
     install_requires=['gym']
